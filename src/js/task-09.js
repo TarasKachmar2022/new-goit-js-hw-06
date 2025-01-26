c22beb215cf279d5e1553f4ff7d6842d4393279a
Taras <kachmar.taras@gmail.com>
2025-01-26 21:15:29 +0200

    add task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,15 @@
+const textEl = document.querySelector(".color");
+const btnEl = document.querySelector(".change-color");
+
+btnEl.addEventListener("click", onBtnClick);
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function onBtnClick() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  textEl.textContent = randomColor;
 }
