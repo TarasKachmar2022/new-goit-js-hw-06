e47cc50e9e3821ef9c5241214e83a7d51d462495
Taras <kachmar.taras@gmail.com>
2025-01-26 15:51:49 +0200

    add task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,43 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const email = e.currentTarget.elements.email.value;
+  const password = e.currentTarget.elements.password.value;
+
+  if (!email || !password) {
+    alert("Всі поля форми мають бути заповнені");
+  } else {
+    createObj(email, password);
+  }
+  e.currentTarget.reset();
+}
+
+function createObj(email, password) {
+  const obj = { email, password };
+  console.log(obj);
+}
+
+// FormData variant
+
+// const formEl = document.querySelector(".login-form");
+
+// formEl.addEventListener("submit", onFormSubmit);
+
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   const email = e.currentTarget.elements.email.value;
+//   const password = e.currentTarget.elements.password.value;
+
+//   if (!email || !password) {
+//     alert("Всі поля форми мають бути заповнені");
+//   } else {
+//     const formData = new FormData(e.currentTarget);
+
+//     formData.forEach((password, email) => console.log(email, password));
+//   }
+
+//   e.currentTarget.reset();
+// }
