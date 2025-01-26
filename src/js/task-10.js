401355a8c5e366b66d29d463041024d7ed0c66a6
Taras <kachmar.taras@gmail.com>
2025-01-26 23:25:44 +0200

    add task-10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,32 @@
+const controlsEl = document.querySelector("#controls");
+const createBtnEl = document.querySelector("[data-create]");
+const destroyBtnEl = document.querySelector("[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+createBtnEl.addEventListener("click", onCreateBtnClick);
+destroyBtnEl.addEventListener("click", destroyBoxes);
+
+function onCreateBtnClick() {
+  const inputValue = Number(controlsEl.firstElementChild.value);
+  createBoxes(inputValue);
+}
+
+function createBoxes(amount) {
+  let arr = [];
+
+  for (let i = 0, j = 30; i < amount; i += 1, j += 10) {
+    const randomColor = getRandomHexColor();
+    const markup = `<div style="width: ${j}px; height: ${j}px; background-color: ${randomColor};">ok</div>`;
+    arr.push(markup);
+  }
+
+  boxesEl.insertAdjacentHTML("beforeend", arr.join(""));
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
